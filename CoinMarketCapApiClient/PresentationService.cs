10ac9e66eefd1d24450b92bd6c762aab529861fc
savaged <savagedinfo@protonmail.com>
2026-01-07 15:34:21 +0000

    wip

--- CoinMarketCapApiClient/PresentationService.cs
+++ CoinMarketCapApiClient/PresentationService.cs
@@ -5,11 +5,12 @@ public static class PresentationService
     public static string Show(Root? root, Watchlist watchlist)
     {
         if (root is null || root.data is null)
-            return "No data available.";
-        var matches = root.data.Join(watchlist,
-            datum => datum.symbol,
-            watched => watched.Key,
-            (datum, watched) => datum);
+            return "No data available!";
+        if (watchlist?.Count == 0)
+            return "Empty watchlist!";
+        var matches = root.data.Where(datum =>
+            watchlist.ContainsKey(datum.symbol)
+            && watchlist[datum.symbol] == datum.name);
         var sb = new System.Text.StringBuilder();
         foreach (var crypto in matches)
             sb.AppendLine($"{crypto.name} ({crypto.symbol}): ${crypto.quote.USD.price:F2}");
