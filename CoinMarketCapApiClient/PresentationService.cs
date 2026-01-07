dcc59644b0c700c454750b435983760046612069
DavidSavage <david.savage@assure-consulting.co.uk>
2026-01-07 15:37:43 +0000

    WIP

--- CoinMarketCapApiClient/PresentationService.cs
+++ CoinMarketCapApiClient/PresentationService.cs
@@ -6,10 +6,11 @@ public static class PresentationService
     {
         if (root is null || root.data is null)
             return "No data available!";
-        if (watchlist?.Count == 0)
+        if (watchlist is null || watchlist.Count == 0)
             return "Empty watchlist!";
         var matches = root.data.Where(datum =>
-            watchlist.ContainsKey(datum.symbol)
+            datum.symbol is not null
+            && watchlist.ContainsKey(datum.symbol)
             && watchlist[datum.symbol] == datum.name);
         var sb = new System.Text.StringBuilder();
         foreach (var crypto in matches)
