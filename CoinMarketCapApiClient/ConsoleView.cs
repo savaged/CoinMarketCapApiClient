ca42cced1e1da7919f7f94ad006a7385f0e5db30
savaged <savagedinfo@protonmail.com>
2026-01-09 09:01:09 +0000

    prep for formatting output

--- CoinMarketCapApiClient/ConsoleView.cs
+++ CoinMarketCapApiClient/ConsoleView.cs
@@ -4,19 +4,13 @@ namespace CoinMarketCapApiClient;
 
 public class ConsoleView : IView
 {
+    private string _canvas = string.Empty;
     public object? DataContext { get; set; }
 
     public void Show()
     {
-        if (DataContext is IIndexViewModel ivm)
-        {
-            if (ivm.Index?.Count() > 0)
-                ConsoleTable.From<Model>(ivm.Index)
-                    .Configure(c => c.NumberAlignment = Alignment.Right)
-                    .Write(Format.MarkDown);
-            else
-                Console.WriteLine(ivm.Status);
-        }
+        Paint();
+        Console.WriteLine(_canvas);
     }
 
     public async Task LoadAsync()
@@ -26,5 +20,19 @@ public class ConsoleView : IView
     }
 
     public void Clear() => Console.Clear();
+
+    private void Paint()
+    {
+        _canvas = string.Empty;
+        if (DataContext is IIndexViewModel ivm)
+        {
+            if (ivm.Index?.Count() > 0)
+                _canvas = ConsoleTable.From<Model>(ivm.Index)
+                    .Configure(c => c.NumberAlignment = Alignment.Right)
+                    .ToMarkDownString();
+            else
+                _canvas = ivm.Status;
+        }
+    }
 
 }
